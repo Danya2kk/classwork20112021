a27b4c4cfde992d7879fbdf413d46574fd2639b8
Danya2kk <90967346+Danya2kk@users.noreply.github.com>
2021-11-20 16:34:25 +0300

    Delete pythonProject3 directory

--- pythonProject3/Task4.py
+++
@@ -1,6 +0,0 @@
-subjects = ["Math", "Physics", "Biology", "Chemistry", "Language", "Geography"]
-
-print(subjects)
-subjectsremove = input("Какие предметы вам не нравятся? ").capitalize()
-subjects.remove(subjectsremove)
-print(subjects)
